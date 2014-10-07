0ec7532399ed8ef406657d9ca1232d9fe74503a7
David Kennedy <David@David-Kennedys-MacBook-Pro.local>
2014-10-07 15:49:56 -0400

    -Added frameworks and added Google Maps API key so to use google maps.

--- MapMyTrip/AppDelegate.m
+++ MapMyTrip/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyDoFk88podjFc3c7kqVT8eZaBRI23-Xjlw"];
     return YES;
 }
 
