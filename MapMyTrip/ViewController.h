cbd18db6f433c5e0f51d242010e906afb214ad1d
David Kennedy <David@David-Kennedys-MacBook-Pro.local>
2014-10-08 13:58:35 -0400

    Added basic map implementation.

--- MapMyTrip/ViewController.h
+++ MapMyTrip/ViewController.h
@@ -7,8 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GoogleMaps/GoogleMaps.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController // <GMSMapViewDelegate>
+@property (strong, nonatomic) IBOutlet GMSMapView *mapView;
 
 
 @end
--- MapMyTrip/ViewController.m
+++ MapMyTrip/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@implementation ViewController {
+    GMSMapView *mapView_;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)viewDidLoad {
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
+
+    
+//    //Controls whether the My Location dot and accuracy circle is enabled.
+//    self.mapView.myLocationEnabled = YES;
+//    //Controls the type of map tiles that should be displayed.
+//    self.mapView.mapType = kGMSTypeNormal;
+//    //Shows the compass button on the map
+//    self.mapView.settings.compassButton = YES;
+//    //Shows the my location button on the map
+//    self.mapView.settings.myLocationButton = YES;
+//    //Sets the view controller to be the GMSMapView delegate
+//    self.mapView.delegate = self;
+//    // - See more at: http:vikrambahl.com/google-maps-ios-xcode-storyboards/#sthash.YvpgnZlY.dpuf
+    
 }
 
 @end
